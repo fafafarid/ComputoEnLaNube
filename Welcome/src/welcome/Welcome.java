c0a7cc068c627ab6c157bd906af156b209ac6c64
fafafarid <72895477+fafafarid@users.noreply.github.com>
2020-10-20 22:42:25 -0500

    Programa de Bienvenido(a) - Primera Modificación

---
+++ Welcome/src/welcome/Welcome.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package welcome;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fafafarid
+ */
+public class Welcome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner dato = new Scanner (System.in);
+        String nom="";
+        System.out.println ("Introduce tu nombre:");
+        nom=dato.nextLine();
+        System.out.println("Bienvenido(a) " + nom);
+    }
+    
+}
